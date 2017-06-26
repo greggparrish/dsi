b550aa959a58b8786653b719a8ecbf0529a3f41b
grp224 <gregg.alb@gmail.com>
2017-06-26 22:41:48 +0200

    add basic styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Dsi"
+    base_title = "Dominican Music in the US"
     if page_title.empty?
       base_title
     else
