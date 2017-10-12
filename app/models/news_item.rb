82b22a2c6d7fd684e3fb66ddd34af9471ac1639a
grp224 <gregg.alb@gmail.com>
2017-10-12 13:51:33 +0200

    add blur, black gradient header

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -20,7 +20,7 @@
 class NewsItem < ApplicationRecord
 	extend FriendlyId
   has_one :header
-  validates :title, :description, presence: true
+  validates :title, :description, :image, presence: true
   accepts_nested_attributes_for :header
   mount_uploader :image, ImagesUploader
 	friendly_id :slug_candidates, use: :slugged
