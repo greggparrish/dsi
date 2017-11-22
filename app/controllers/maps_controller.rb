7a02e560f9cbd859768aa7fc852e13a16d6eee4a
grp224 <gregg.alb@gmail.com>
2017-11-22 14:58:59 +0100

    add open specific map infowindow from url param

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,6 +5,7 @@ class MapsController < ApplicationController
     authorize @maps
     @hash = Gmaps4rails.build_markers(@maps) do |map, marker|
       marker.infowindow render_to_string(partial: "/maps/infowindow", locals:  { object: map})
+      marker.json({ id: map.id })
       marker.lat map.latitude
       marker.lng map.longitude
       marker.title map.title
